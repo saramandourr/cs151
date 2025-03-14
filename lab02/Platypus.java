75b52733d0ab7e4bfb4a10f98832fd632616753c
saramandourr <sarahh.mandour17@gmail.com>
2025-03-13 23:19:55 -0400

    Create Platypus.java

---
+++ lab02/Platypus.java
@@ -0,0 +1,6 @@
+public class Platypus extends Mammal {
+    public Platypus() { super("Platypus", "errrr"); }
+    public boolean laysEggs() { return true; }
+    public boolean livesInWater() { return true; }
+}
+
